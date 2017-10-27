b45cdbd503124179644e35a8ca93f95ed5b88eff
Giovanni Filippi <giovannif23@gmail.com>
2017-10-26 20:20:00 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry'
+
+export default class App extends React.Component {
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    );
+  }
+}
